75df758553eedf9d0efa72f3bb2f5664db603811
vagrant <vagrant@vagrant.vm>
2018-11-18 23:09:18 +0000

    log_analysis.py added

---
+++ logs_analysis.py
@@ -0,0 +1,8 @@
+import psycopg2
+
+db = psycopg2.connect("dbname=news")
+cursor = db.cursor()
+cursor.execute("select articles.title, count(*) as new from articles left join log on concat('/article/',articles.slug) = log.path group by articles.title order by new desc limit 3")
+results = cursor.fetchall()
+print(results)
+db.close()
