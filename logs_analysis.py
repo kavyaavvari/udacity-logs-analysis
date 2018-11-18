bcfba8259f2ecae82b0dcb43b1f68700f5897fa2
Kavya Avvari <44819195+kavyaavvari@users.noreply.github.com>
2018-11-18 18:21:30 -0500

    Update logs_analysis.py

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,6 +1,7 @@
 import psycopg2
 
 print('The most popular three articles of all time:')
+
 db = psycopg2.connect("dbname=news")
 cursor = db.cursor()
 cursor.execute("select articles.title, count(*) as new from articles left join log on concat('/article/',articles.slug) = log.path group by articles.title order by new desc limit 3")
