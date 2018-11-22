00c337934518395945cb62f870d1630fa9541173
Kavya Avvari <44819195+kavyaavvari@users.noreply.github.com>
2018-11-22 18:55:08 -0500

    Update logs_analysis.py

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,28 +1,54 @@
 import psycopg2
 
-print('The most popular three articles of all time:')
+DBNAME = "news"
 
-db = psycopg2.connect("dbname=news")
-cursor = db.cursor()
-cursor.execute("SELECT articles.title, count(*) AS new FROM articles left JOIN log on concat('/article/',articles.slug) = log.path GROUP BY articles.title ORDER BY new DESC limit 3")
+def write_text(file_name, text):
+    with open(file_name,"a+") as f:
+        f.write(str(text))
+
+
+
+question_1 = "The most popular three articles of all time:"
+
+query_1 = ("SELECT articles.title" 
+           "COUNT(*) AS new FROM articles" 
+           "left JOIN log on concat('/article/',articles.slug) = log.path group" 
+           "BY articles.title" 
+           "ORDER BY new DESC" 
+           "LIMIT 3")
+
+results = cursor.fetchall()
+print(results)
+
+write_text("output.txt", results)
+
+
+question_2 = "The most popular article authors of all time:"
+
+quergy_2 = ("SELECT authors.name"
+            "COUNT(*) as views" 
+            "FROM articles, authors, log"
+            "WHERE  concat('/article/',articles.slug) = log.path" 
+            "AND articles.author = authors.id"  
+            "GROUP BY authors.name" 
+            "ORDER BY views DESC")
+
+results = cursor.fetchall()
+print(results)
+
+write_text("output.txt", results)
+
+
+question_3 = "Days where more than 1% of requests lead to errors:"
+
+query_3 = ("WITH date_percentage" 
+           "AS (WITH date_status AS (SELECT to_char(time,'DD-MON-YYYY') AS date, status FROM log)" 
+           "SELECT date, sum(case when status = '404 NOT FOUND' then 1.0 else 0 end)/count(date)*100 AS percent" 
+           "FROM date_status GROUP BY date)" 
+           "SELECT * FROM date_percentage" 
+           "WHERE percent > '1.0'")
 results = cursor.fetchall()
 print(results)
 db.close()
 
-
-print('The most popular article authors of all time:')
-
-cursor = db.cursor()
-cursor.execute("SELECT authors.name, count(*) AS views FROM articles, authors, log WHERE  concat('/article/',articles.slug) = log.path AND articles.author = authors.id  GROUP BY authors.name ORDER BY views DESC")
-results = cursor.fetchall()
-print(results)
-
-
-print('Days with more than 1% of requests leading to errors:')
-
-cursor = db.cursor()
-cursor.execute("WITH date_percentage AS (WITH date_status AS (SELECT to_char(time,'DD-MON-YYYY') AS date, status FROM log) SELECT date, sum(case when status = '404 NOT FOUND' then 1.0 else 0 end)/count(date)*100 AS percent FROM date_status GROUP BY date) SELECT * FROM date_percentage WHERE percent > '1.0'")
-results = cursor.fetchall()
-print(results)
-
-db.close()
+write_text("output.txt", results)
